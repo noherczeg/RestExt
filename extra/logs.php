cad1d2c6b569c7cf70ddd4685c9e3320cfc984dd
Norbert Csaba Herczeg <noherczeg@gmail.com>
2013-11-29 20:40:43 +0100

    formatted for github markdown

--- extra/logs.php
+++ extra/logs.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Queue;
 
-/**
+/*
 |--------------------------------------------------------------------------
 | Database Logging
 |--------------------------------------------------------------------------
